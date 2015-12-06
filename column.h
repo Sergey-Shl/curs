7387156fd26d6774344b3b73b2c066e8e4aec06a
Sergey-Shl <Sergeishlyapnikov@gmail.com>
2015-12-06 19:45:54 +0300

    Update column.h

--- column.h
+++ column.h
@@ -17,10 +17,13 @@ public:
 	inline void push(const vehicle&);     // поместить элемент в вершину стека
 	inline vehicle pop();                   // удалить элемент из вершины стека и вернуть его
 	inline void printStack();         // вывод стека на экран
-	inline const vehicle &Peek(int) const; // n-й элемент от вершины стека
+	inline vehicle &Peek(int); // n-й элемент от вершины стека
 	inline int getStackSize() const;  // получить размер стека
 	inline vehicle *getPtr() const;         // получить указатель на стек
 	inline int getTop() const;        // получить номер текущего элемента в стеке
+
+	inline void checkSpeed();
+	inline float checkDis(int, int);
 };
 
 // реализация методов шаблона класса STack
@@ -64,10 +67,10 @@ inline vehicle column::pop()
 }
 
 // функция возвращает n-й элемент от вершины стека
-inline const vehicle& column::Peek(int nom) const
+inline vehicle& column::Peek(int nom) 
 {
 	if(nom <= top)
-		return stackPtr[top - nom]; // вернуть n-й элемент стека
+		return stackPtr[nom]; // вернуть n-й элемент стека
 }
 
 // вывод стека на экран
@@ -93,4 +96,44 @@ inline vehicle* column::getPtr() const
 inline int column::getTop() const
 {
 	return top;
+}
+
+inline float column::checkDis(int c2, int c1 = 0)
+{
+	if (c2 <= getTop())
+	{
+		vehicle const &car1 = Peek(c1);
+		vehicle const &car2 = Peek(c2);
+
+		return sqrt(pow((car1.x - car2.x), 2) + pow((car1.y - car2.y), 2));
+	}
+	return 0.0;
+}
+
+inline void column::checkSpeed()
+{
+	float dv = 0.0002; // v = 0.05
+	vehicle car1 = Peek(0);
+	for (int i = 1; i < top; i++)
+	{
+		vehicle car1 = Peek(i - 1);
+		vehicle &car2 = Peek(i);
+		if (checkDis(i, i - 1) > (car1.height / 2 + car2.height / 2))
+		{
+			car2.speed.y += dv;
+			/*
+			if (car1.speed.x - dv > car2.speed.x)
+				car2.speed.x += dv;
+			if (car1.speed.x + dv < car2.speed.x)
+				car2.speed.x -= dv;
+			if (car1.speed.y - dv > car2.speed.y)
+				car2.speed.y += dv;
+			if (car1.speed.y + dv < car2.speed.y)
+				car2.speed.y -= dv;
+			*/
+		}
+		else
+			car2.speed.y = 0;
+	}
+
 }
