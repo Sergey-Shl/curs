a94f2bf347516dd9c6659d9444eaa6bff13522a5
Sergey-Shl <Sergeishlyapnikov@gmail.com>
2016-01-07 14:30:04 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
-#include <GL\glew.h>
-#include <GL\freeglut.h>
-
+#include <gl/glew.h>
+#include <gl/freeglut.h>
 #include <iostream>
-
-#include <glm\glm.hpp>
-#include <glm\ext.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
 
 #include "loadshaders.h"
 #include "vehicle.h"
 #include "column.h"
-#include "road.h"
 
 using namespace std;
 
@@ -22,11 +17,10 @@ GLuint shaderprogram;
 GLuint MatrixID;
 glm::mat4 MVP;
 
-const int max_veh = 2;
+const int max_veh = 10;
 
 column c(max_veh);
 
-road map;
 
 glm::vec3 rotate(glm::vec3 cpoint, glm::vec3 point, float angle){
 	glm::vec3 rotated_point;
@@ -38,7 +32,6 @@ glm::vec3 rotate(glm::vec3 cpoint, glm::vec3 point, float angle){
 
 void init(void)
 {
-
 	glGenVertexArrays(1, &VA);
 	glBindVertexArray(VA);
 
@@ -102,12 +95,7 @@ void display(void)
 		glDrawArrays(GL_LINE_STRIP, ((i + 1) * 8) - 2, 2);
 	}
 
-	int index = (c.getTop() * 8) + 1;
-
-	glDrawArrays(GL_LINE_STRIP, index, 2);
-	glDrawArrays(GL_LINE_STRIP, index + 2, 2);
-	glDrawArrays(GL_LINE_STRIP, index + 2 + 2, 2);
-	glDrawArrays(GL_LINE_STRIP, index + 2 + 2 + 2, 2);
+	
 
 	glDisableVertexAttribArray(0);
 	glDisableVertexAttribArray(1);
@@ -167,15 +155,21 @@ float b = 0;
 float dx, dy, tx, ty;
 void move()
 {
-	//vec = normolize(glm::vec3(c.Peek(i).direction.x + b, c.Peek(i).direction.y + a, 0.0));
 	for (int i = 0; i < c.getTop(); i++)
 	{
-		//c.Peek(i).direction = normolize(glm::vec3(c.Peek(i).direction.x, c.Peek(i).direction.y, 0.0));;
+		if (c.Peek(i).y > 40)
+			c.Peek(i).direction.x += 0.01;
 
-		if (i % 2 == 0)
-			c.Peek(i).direction = normolize(glm::vec3(c.Peek(i).direction.x , c.Peek(i).direction.y, 0.0));
-		else
-			c.Peek(i).direction = normolize(glm::vec3(c.Peek(i).direction.x, c.Peek(i).direction.y, 0.0));
+		if (c.Peek(i).x > 40)
+			c.Peek(i).direction.y -= 0.01;
+
+		if (c.Peek(i).x > 40 && c.Peek(i).y < -20)
+		{
+			c.Peek(i).direction.y += 0.01;
+			c.Peek(i).direction.x -= 0.01;
+		}
+
+		c.Peek(i).direction = normolize({ c.Peek(i).direction.x, c.Peek(i).direction.y, 0.0 });
 
 		c.Peek(i).x += c.Peek(i).direction.x * c.Peek(i).speed;
 		c.Peek(i).y += c.Peek(i).direction.y * c.Peek(i).speed;
@@ -186,38 +180,21 @@ void move()
 		}
 
 		float angle = c.Peek(i).getAngle();
-
-		cout << i << " : " << angle << endl;
-
-		tx = dx = tx - c.Peek(i).direction.x;
-		ty = dy = ty - c.Peek(i).direction.y;
-
-		cout << i << " : dX: " << dx << endl;
-		cout << i << " : dY: " << dy << endl;
-
-		{
-			for (int j = 0; j < 4; j++)
-			{
-				glm::vec3 v;
-
-				v = glm::vec3(c.Peek(i).xCoord[j], c.Peek(i).yCoord[j], 0.0);
-
-					v = rotate(glm::vec3(c.Peek(i).x, c.Peek(i).y, 0.0), v, angle);
-
-				c.Peek(i).xCoord[j] = v.x;
-				c.Peek(i).yCoord[j] = v.y;
-			}
-		}
+		c.Peek(i).rotate(angle);
 
 	}
 }
 
 void timer(int = 0)
 {
-		a -= 0.0001;
-		b -= 0.00009;
+	//a -= 0.0001;
 	
-		
+	if (b < - 0.03)
+		a += 0.001;
+	else
+		a -= 0.0001;
+	
+	b -= 0.00009;
 
 	int const numVertex = 8 * 3;
 	GLfloat vertex[max_veh * numVertex + 12];
@@ -263,7 +240,7 @@ void timer(int = 0)
 		if (i == c.getTop() - 1)
 		{
 			int index = (i * numVertex) + j;
-			vertex[index++] = 5/ 10;
+			vertex[index++] = 5 / 10;
 			vertex[index++] = 0;
 			vertex[index++] = 2 / 10;
 			vertex[index++] = -5;
@@ -277,28 +254,10 @@ void timer(int = 0)
 			vertex[index++] = 5;
 		}
 
-		/*
-		if (i == c.getTop() - 1)
-		{
-			int index = (i * numVertex) + j;
-			for (int x = 0; x < 200; x++)
-			{
-				for (int y = 0; y < 200; y++)
-				{
-					if (map.map[x][y] == 1)
-						vertex[index] = map.map[x][y];
-				}
-			}
-			
-		}
-		*/
+	
 		j = 0;
 
-		//float s = c.Peek(i).direction.length();
 
-		//cout << "========= Car " << i << " =========" << endl;
-		//cout << "x" << i << ": " << x << " y" << i << ": " << y << endl;
-		//cout << "Angle: " << car.getAngle() * 180 / 3.14 << endl;
 	}
 
 	glBindBuffer(GL_ARRAY_BUFFER, VertextBuffer);
@@ -308,6 +267,12 @@ void timer(int = 0)
 
 	display();
 
+	for (int i = 1; i < c.getTop(); i++)
+	{
+		//c.Peek(i).direction = normolize(glm::vec3(c.Peek(i).direction.x + a, c.Peek(i).direction.y + b, 0.0));
+		c.Peek(i).checkDis(c.Peek(i - 1));
+	}
+	
 	/*	if (c.Peek(0).y > 35)
 	c.Peek(0).direction *= c.Peek(0).deceleration;
 	else
@@ -322,9 +287,9 @@ void timer(int = 0)
 	glutTimerFunc(5, timer, 0);
 }
 
-vehicle car1(5.0, 0.0, 0.1, glm::vec3(0.0, 1.0, 0.0), 1.0, 0.99, 4.0, 9.0, 1500.0, 0, 0);
-vehicle car2(15.0, 0.0, 0.2, glm::vec3(0.0, 1.0, 0.0), 1.0, 0.7, 3.0, 6.0, 1500.0, 1, 1);
-vehicle car3(10.0, 0.0, 0.21, glm::vec3(0.0, 1.0, 0.0), 1.0, 0.7, 3.0, 6.0, 1500.0, 2, 2);
+vehicle car1(5.0, 0.0, 0.1, glm::vec3(0.0, 1.0, 0.0), 0.001, 0.0009, 4.0, 9.0, 1500.0, 0, 0);
+vehicle car2(5.0, -20, 0.2, glm::vec3(0.0, 1.0, 0.0), 0.001, 0.0009, 3.0, 6.0, 1500.0, 1, 1);
+vehicle car3(5.0, -46, 0.3, glm::vec3(0.0, 1.0, 0.0), 0.001, 0.0009, 3.0, 6.0, 1500.0, 2, 2);
 vehicle car4(-10.0, 0.0, 0.25, glm::vec3(0.0, 1.0, 0.0), 1.0, 0.99, 2.0, 5.0, 1500.0, 0, 0);
 vehicle car5(-5.0, 0.0, 0.2, glm::vec3(0.0, 1.0, 0.0), 4.0, 0.0, 2.5, 4.0, 1500.0, 4, 4);
 
@@ -340,15 +305,17 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < max_veh; i++)
 	{
 		cars[i].x = (i - max_veh / 2) * 0 - 5;
-		cars[i].y = - 100 - (i - max_veh / 2) * 10;
-		cars[i].speed = +0.2;
+		cars[i].y = -50 - (i - max_veh / 2) * 10;
+		//cars[i].y = 0;
+		cars[i].speed = +0.15;
 
-		cars[i].direction = { 0.0, 1.0, 0.0 };
+		cars[i].direction = normolize({ 0.0, 1.0, 0.0 });
+		cars[i].acceleration = 0.001;
+		cars[i].deceleration = 0.0009;
 		cars[i].width = 3;
 		cars[i].height = 6;
 		cars[i].idVehicle = i;
 		cars[i].position = i;
-
 		float w2 = cars[i].width / 2;
 		float h2 = cars[i].height / 2;
 
@@ -382,6 +349,7 @@ int main(int argc, char** argv) {
 
 			cars[i].xCoord[3] = cars[i].x - w2 - 1;
 			cars[i].yCoord[3] = cars[i].y + h2;
+			cout << "" << endl;
 		}
 
 
@@ -389,14 +357,13 @@ int main(int argc, char** argv) {
 		c.push(cars[i]);
 	}
 
-	//c.Peek(0).direction = { -0.5, 1.0, 0.0 };
-
+	//c.Peek(0).speed = 0.2;
 	//c.printStack();
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(1024, 1024);
-	glutInitContextVersion(4, 3);
+	glutInitContextVersion(3, 0);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
 	glutCreateWindow(argv[0]);
 	glewExperimental = true;
